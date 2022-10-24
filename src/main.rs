db91eeddb6c49cafbde97b1174e95ac0a1ee7aef
Zxilly <zhouxinyu1001@gmail.com>
2022-10-25 03:04:55 +0800

    fix: fix tokio runtime

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 use clap::{Command, crate_authors, crate_version};
-use tokio::runtime::Handle;
+use tokio::runtime::Runtime;
 
 mod send;
 
 fn main() {
     let matches = Command::new("ZNotify")
+        .bin_name("znotify")
+        .display_name("ZNotify")
         .author(crate_authors!())
         .version(crate_version!())
         .about("Send notification to yourself.")
+        .arg_required_else_help(true)
         .subcommand(send::send_command())
         .get_matches();
     match matches.subcommand() {
         Some(("send", sub_matches)) =>
             {
-                Handle::current().block_on(send::send_action(sub_matches));
+                Runtime::new().unwrap().block_on(send::send_action(sub_matches));
             }
         _ => {}
     }
